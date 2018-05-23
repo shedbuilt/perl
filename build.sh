e326405a32607d57b3fd934d3c7792006626c616
Auston Stewart <auston@shedbuilt.net>
2018-05-22 17:03:52 -1000

    Updating for blank package options

--- build.sh
+++ build.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
-case "$SHED_BUILD_MODE" in
-    toolchain)
-        sh Configure -des -Dprefix=/tools -Dlibs=-lm &&
-        make -j $SHED_NUM_JOBS &&
-        mkdir -pv "${SHED_FAKE_ROOT}/tools/bin" &&
-        cp -v perl cpan/podlators/scripts/pod2man "${SHED_FAKE_ROOT}/tools/bin" &&
-        mkdir -pv "${SHED_FAKE_ROOT}/tools/lib/perl5/${SHED_PKG_VERSION}" &&
-        cp -Rv lib/* "${SHED_FAKE_ROOT}/tools/lib/perl5/${SHED_PKG_VERSION}"
-        ;;
-    *)
-        if [ ! -e /etc/hosts ]; then
-            echo "127.0.0.1 localhost $(hostname)" > /etc/hosts
-        fi
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_PREFIX='/usr'
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    SHED_PKG_LOCAL_PREFIX='/tools'
+fi
+# Configure, Build and Install
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    sh Configure -des -Dprefix=${SHED_PKG_LOCAL_PREFIX} \
+                      -Dlibs=-lm &&
+    make -j $SHED_NUM_JOBS &&
+    mkdir -pv "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_PREFIX}/bin" &&
+    cp -v perl cpan/podlators/scripts/pod2man "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_PREFIX}/bin" &&
+    mkdir -pv "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_PREFIX}/lib/perl5/${SHED_PKG_VERSION}" &&
+    cp -Rv lib/* "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_PREFIX}/lib/perl5/${SHED_PKG_VERSION}" || exit 1
+else
+    # HACK: Perl depends on the presence of a hosts file
+    if [ ! -e /etc/hosts ]; then
+        echo "127.0.0.1 localhost $(hostname)" > /etc/hosts
+    fi
         export BUILD_ZLIB=False
         export BUILD_BZIP2=0
-        sh Configure -des -Dprefix=/usr                 \
-                          -Dvendorprefix=/usr           \
-                          -Dman1dir=/usr/share/man/man1 \
-                          -Dman3dir=/usr/share/man/man3 \
-                          -Dpager="/usr/bin/less -isR"  \
-                          -Duseshrplib                  \
+        sh Configure -des -Dprefix=${SHED_PKG_LOCAL_PREFIX} \
+                          -Dvendorprefix=${SHED_PKG_LOCAL_PREFIX} \
+                          -Dman1dir=${SHED_PKG_LOCAL_PREFIX}/share/man/man1 \
+                          -Dman3dir=${SHED_PKG_LOCAL_PREFIX}/share/man/man3 \
+                          -Dpager="/usr/bin/less -isR" \
+                          -Duseshrplib \
                           -Dusethreads &&
         make -j $SHED_NUM_JOBS &&
-        make DESTDIR="$SHED_FAKE_ROOT" install
-        ;;
-esac
+        make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+fi
